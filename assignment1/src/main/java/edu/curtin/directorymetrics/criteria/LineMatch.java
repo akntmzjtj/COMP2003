186a004b46951a421e931229086b6d642a016437
akntmzjtj <102938757+akntmzjtj@users.noreply.github.com>
2025-09-02 12:50:18 +0800

    added javadoc comments

--- assignment1/src/main/java/edu/curtin/directorymetrics/criteria/LineMatch.java
+++ assignment1/src/main/java/edu/curtin/directorymetrics/criteria/LineMatch.java
@@ -1,10 +1,20 @@
 package edu.curtin.directorymetrics.criteria;
 
+/**
+ * A class used to store the line number and the contents of the line from a
+ * file.
+ */
 public class LineMatch
 {
     private final int lineNumber;
     private final String content;
 
+    /**
+     * Constructor for LineMatch class
+     *
+     * @param lineNumber
+     * @param content
+     */
     public LineMatch(int lineNumber, String content)
     {
         // Should not be changed after instantiation
@@ -12,11 +22,19 @@ public class LineMatch
         this.content = content;
     }
 
+    /**
+     * Getter for lineNumber
+     * @return lineNumber
+     */
     public int getLineNumber()
     {
         return lineNumber;
     }
 
+    /**
+     * Getter for content of line
+     * @return content
+     */
     public String getContent()
     {
         return content;
