5979454948a62c20b0b003594cf27a6a2bb1cf03
akntmzjtj <102938757+akntmzjtj@users.noreply.github.com>
2025-09-02 12:31:48 +0800

    exception for Criteria. Removed unchecked exceptions from throws clause

---
+++ assignment1/src/main/java/edu/curtin/directorymetrics/criteria/CriteriaException.java
@@ -0,0 +1,28 @@
+package edu.curtin.directorymetrics.criteria;
+
+/**
+ * Exception for Criteria
+ */
+public class CriteriaException extends Exception
+{
+    /**
+     * Constructs Criteria exception using given message.
+     *
+     * @param msg The message as to why the exception was thrown.
+     */
+    public CriteriaException(String msg)
+    {
+        super(msg);
+    }
+
+    /**
+     * Constructs Criteria exception using a given message and includes a trail
+     *
+     * @param msg The message as to why the exception was thrown.
+     * @param cause For exception chaining
+     */
+    public CriteriaException(String msg, Throwable cause)
+    {
+        super(msg, cause);
+    }
+}
