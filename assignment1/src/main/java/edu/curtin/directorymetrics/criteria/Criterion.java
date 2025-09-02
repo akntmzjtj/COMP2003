258f7a382a20cfe4a7071f33f3f87e434c3ebd50
akntmzjtj <102938757+akntmzjtj@users.noreply.github.com>
2025-09-02 10:56:32 +0800

    added comment

--- assignment1/src/main/java/edu/curtin/directorymetrics/criteria/Criterion.java
+++ assignment1/src/main/java/edu/curtin/directorymetrics/criteria/Criterion.java
@@ -1,5 +1,9 @@
 package edu.curtin.directorymetrics.criteria;
 
+/**
+ * Interface for Criterion subclasses. Must implement a function for finding a
+ * match in the given sample depending on the pattern defined at construction.
+ */
 public interface Criterion
 {
     boolean findMatch(String sample);
