844ad51c0b0b2ddbca5bbb2bca1fca6c00baa64b
akntmzjtj <102938757+akntmzjtj@users.noreply.github.com>
2025-09-02 12:44:17 +0800

    added javadoc strings

--- assignment1/src/main/java/edu/curtin/directorymetrics/criteria/CriterionExpression.java
+++ assignment1/src/main/java/edu/curtin/directorymetrics/criteria/CriterionExpression.java
@@ -4,16 +4,34 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
+/**
+ * A class that implements Criterion. Uses Pattern and Matcher to find a match
+ * in a given string (a line from a file). The pattern (regex) is defined at the
+ * construction of the object.
+ */
 public class CriterionExpression implements Criterion
 {
     private Pattern p;
 
+    /**
+     * Contructs object by using regex defined by expression
+     *
+     * @param expression the string that contains the regex
+     * @throws PatternSyntaxException when Pattern object cannot compile given
+     *                                string
+     */
     public CriterionExpression(String expression) throws PatternSyntaxException
     {
         this.p = Pattern.compile(expression);
     }
 
     @Override
+    /**
+     * Checks if the contents of the line matches the pattern defined at
+     * construction.
+     *
+     * @param sample Contains line from a file
+     */
     public boolean findMatch(String sample)
     {
         // Create a Matcher object and pass 'sample'
