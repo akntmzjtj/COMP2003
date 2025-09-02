53cbea3ed11284c0ac9689a96f2bb8966688eb90
akntmzjtj <102938757+akntmzjtj@users.noreply.github.com>
2025-09-02 12:58:10 +0800

    added javadoc comments

--- assignment1/src/main/java/edu/curtin/directorymetrics/criteria/CriterionText.java
+++ assignment1/src/main/java/edu/curtin/directorymetrics/criteria/CriterionText.java
@@ -1,15 +1,30 @@
 package edu.curtin.directorymetrics.criteria;
 
+/**
+ * A class that implements Criterion. Just stores plain text to be checked
+ * against.
+ */
 public class CriterionText implements Criterion
 {
 
     private String text;
 
+    /**
+     * Constructor for CriterionText class.
+     *
+     * @param text Text used to be checked again.
+     */
     public CriterionText(String text)
     {
         this.text = text;
     }
 
+    /**
+     * Checks if the contents of the line contains the string defined at
+     * construction.
+     *
+     * @param sample Contains line from a file
+     */
     @Override
     public boolean findMatch(String sample)
     {
