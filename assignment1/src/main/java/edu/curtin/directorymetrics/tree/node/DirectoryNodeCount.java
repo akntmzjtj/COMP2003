2c945053fa6688f8d03993df1dfc956c32740f8c
akntmzjtj <102938757+akntmzjtj@users.noreply.github.com>
2025-09-05 12:55:52 +0800

    added javadoc comments

--- assignment1/src/main/java/edu/curtin/directorymetrics/tree/node/DirectoryNodeCount.java
+++ assignment1/src/main/java/edu/curtin/directorymetrics/tree/node/DirectoryNodeCount.java
@@ -2,17 +2,33 @@ package edu.curtin.directorymetrics.tree.node;
 
 import java.io.File;
 
+/**
+ * Implementation of DirectoryNode composite template. Format of the name
+ * involves the total number of matches as well.
+ */
 public class DirectoryNodeCount extends DirectoryNode
 {
+    /**
+     * Constructor for DirectoryNodeCount.
+     *
+     * @param file File object that should point to a directory. Template class
+     *             handles when it is not
+     */
     public DirectoryNodeCount(File file)
     {
         super(file);
     }
 
+    /**
+     * Formats the name of the directory. Adds number of total matches at the
+     * end.
+     *
+     * @param indent The number of whitespace to print before the content.
+     * @return formatted string
+     */
     @Override
     public String formatName(String indent)
     {
-        return indent + getFile().getName() + ": "
-            + getMatchesCount();
+        return indent + getFile().getName() + ": " + getMatchesCount();
     }
 }
