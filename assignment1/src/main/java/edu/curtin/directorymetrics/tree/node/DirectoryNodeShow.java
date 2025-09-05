35140b911af04dda11a464bf83a319ff91a6d65e
akntmzjtj <102938757+akntmzjtj@users.noreply.github.com>
2025-09-05 12:57:37 +0800

    added javadoc comments

--- assignment1/src/main/java/edu/curtin/directorymetrics/tree/node/DirectoryNodeShow.java
+++ assignment1/src/main/java/edu/curtin/directorymetrics/tree/node/DirectoryNodeShow.java
@@ -2,13 +2,29 @@ package edu.curtin.directorymetrics.tree.node;
 
 import java.io.File;
 
+/**
+ * Implementation of DirectoryNode composite template. Format of the name only
+ * involves the name of the 'file'.
+ */
 public class DirectoryNodeShow extends DirectoryNode
 {
+    /**
+     * Constructor for DirectoryNodeShow.
+     *
+     * @param file File object that should point to a directory. Template class
+     *             handles when it is not
+     */
     public DirectoryNodeShow(File file)
     {
         super(file);
     }
 
+    /**
+     * Formats the name of the directory. Only shows the name of the directory.
+     *
+     * @param indent The number of whitespace to print before the content.
+     * @return formatted string
+     */
     @Override
     public String formatName(String indent)
     {
