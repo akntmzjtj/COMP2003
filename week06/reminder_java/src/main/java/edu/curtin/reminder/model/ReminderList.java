13f72bb5aabbc1e2b4f1d705a514ba5feabefd0c
akntmzjtj <102938757+akntmzjtj@users.noreply.github.com>
2025-09-12 10:53:58 +0800

    initial commit for worksheet06

---
+++ week06/reminder_java/src/main/java/edu/curtin/reminder/model/ReminderList.java
@@ -0,0 +1,42 @@
+package edu.curtin.reminder.model;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Represents a collection of Reminders.
+ */
+public class ReminderList
+{
+    private List<Reminder> reminders;
+
+    public ReminderList()
+    {
+        reminders = new ArrayList<>();
+    }
+    
+    /** Add a single reminder to the list. */
+    public void addReminder(Reminder rem)
+    {
+        reminders.add(rem);
+    }
+    
+    /** Add a complete list of reminders to the existing list. */
+    public void addReminders(List<Reminder> newReminders)
+    {
+        reminders.addAll(newReminders);
+    }
+    
+    /** Remove a reminder by index (i.e. 0 to #reminders-1) */
+    public void removeReminder(int index)
+    {
+        reminders.remove(index);
+    }
+    
+    /** Retrieve a copy of the reminder list. */
+    public List<Reminder> getReminders()
+    {
+        return Collections.unmodifiableList(reminders);
+    }
+}
