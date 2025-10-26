8bbacd8bdb0431daab4bc9fe7540c58e92bd59eb
akntmzjtj <102938757+akntmzjtj@users.noreply.github.com>
2025-10-26 19:45:05 +0800

    assignment2: added javadoc commenting

--- assignment2/src/main/java/edu/curtin/oose/assignment2/probe/Probe.java
+++ assignment2/src/main/java/edu/curtin/oose/assignment2/probe/Probe.java
@@ -13,6 +13,8 @@ import edu.curtin.oose.assignment2.probe.command.Command;
  * It keeps track of its location and can display what it's currently measuring.
  * It's mainly a simulation and cannot instruct a physical probe to move or to
  * return actual measurements.
+ *
+ * @author Joshua Orbon 20636948
  */
 public class Probe implements DiagnosticObserver
 {
@@ -22,8 +24,8 @@ public class Probe implements DiagnosticObserver
     protected static final ProbeState MEASURING_STATE = new MeasuringState();
 
     private String name;
-    private double currentLat;
-    private double currentLong;
+    private double lat;
+    private double longi;
     private final double maxDistance; // max travel distance per move
                                       // instruction
     private ProbeState state;
@@ -31,6 +33,17 @@ public class Probe implements DiagnosticObserver
     private List<Command> commands;
     private List<String> commandHistory;
 
+    /**
+     * Constructor for Probe.
+     *
+     * @param name        Name of the probe
+     * @param lat         Initial lattitude of probe
+     * @param longi       Initial longitude of probe
+     * @param maxDistance Max distance a probe can travel per sol
+     * @throws IllegalArgumentException When name provided is null or blank,
+     *                                  maxDistance is less than or equal to
+     *                                  zero
+     */
     public Probe(String name, double lat, double longi, double maxDistance)
     {
         if(name == null)
@@ -49,71 +62,138 @@ public class Probe implements DiagnosticObserver
         }
 
         this.name = name;
+        this.lat = lat;
+        this.longi = longi;
+        this.maxDistance = maxDistance;
         this.state = LOW_POWER_STATE;
+
+        // Initialise list of current commands and command history
         this.commands = new LinkedList<>();
         this.commandHistory = new LinkedList<>();
-
-        // set current coordinates
-        this.currentLat = lat;
-        this.currentLong = longi;
-        this.maxDistance = maxDistance;
     }
 
+    /**
+     * Getter for name of the probe.
+     *
+     * @return name of probe
+     */
     public String getName()
     {
         return this.name;
     }
 
+    /**
+     * Getter for probe's current lattitude.
+     *
+     * @return lat
+     */
     public double getLattitude()
     {
-        return currentLat;
+        return lat;
     }
 
+    /**
+     * Getter for probe's current longitude.
+     *
+     * @return longi
+     */
     public double getLongitude()
     {
-        return currentLong;
+        return longi;
     }
 
+    /**
+     * Getter for the max travel distance the probe can travel per sol.
+     *
+     * @return
+     */
     public double getMaxDistance()
     {
         return this.maxDistance;
     }
 
-    public String getState()
+    /**
+     * Protected getter for probe's current state.
+     *
+     * @return state but String
+     */
+    protected String getState()
     {
         return this.state.getState();
     }
 
+    /**
+     * Getter for Probe object's current list of commands.
+     *
+     * @return List of Command objects
+     */
     protected List<Command> getCommands()
     {
         return Collections.unmodifiableList(this.commands);
     }
 
+    /**
+     * Getter for Probe object's command history.
+     *
+     * @return List of Strings detailing probe's status per sol
+     */
     public List<String> getHistory()
     {
         return Collections.unmodifiableList(this.commandHistory);
     }
 
+    /**
+     * Setter for probe's lattitude.
+     *
+     * @param lat New lattitude
+     */
     public void setLattitude(double lat)
     {
-        this.currentLat = lat;
+        this.lat = lat;
     }
 
+    /**
+     * Setter for probe's longitude.
+     *
+     * @param longi New longitude
+     */
     public void setLongitude(double longi)
     {
-        this.currentLong = longi;
+        this.longi = longi;
     }
 
+    /**
+     * Setter for probe's state.
+     *
+     * @param state ProbeState object
+     * @throws IllegalArgumentException When ProbeState is null
+     */
     protected void setState(ProbeState state)
     {
+        if(state == null)
+        {
+            throw new IllegalArgumentException("Parameter is null.");
+        }
+
         this.state = state;
     }
 
+    /**
+     * Setter for probe's list of commands
+     *
+     * @param commands List of Command objects
+     */
     public void setCommands(List<Command> commands)
     {
         this.commands = commands;
     }
 
+    /**
+     * Stores move commands depending on state.
+     *
+     * @param moves List of Command objects
+     * @throws IllegalArgumentException When List is null or contains null
+     */
     public void storeMoves(List<Command> moves)
     {
         if(moves == null)
@@ -129,6 +209,12 @@ public class Probe implements DiagnosticObserver
         this.state.storeMoves(this, moves);
     }
 
+    /**
+     * Stores measure commands depending on state
+     *
+     * @param measureList List of Command objects
+     * @throws IllegalArgumentException When List is null or contains null
+     */
     public void storeMeasure(List<Command> measureList)
     {
         if(measureList == null)
@@ -144,17 +230,29 @@ public class Probe implements DiagnosticObserver
         this.state.storeMeasure(this, measureList);
     }
 
+    /**
+     * Simulates the command being sent from the satellite to the probe.
+     *
+     * @param sol Current sol (Martian day)
+     */
     public void sendCommand(int sol)
     {
         // If empty, set to low state mode
         if(this.commands.isEmpty())
         {
-            this.state = Probe.LOW_POWER_STATE;
+            this.state = LOW_POWER_STATE;
         }
         else
         {
             // Remove first command from list and print
             Command c = this.commands.removeFirst();
+
+            // For whatever reason...
+            if(c == null)
+            {
+                throw new IllegalStateException(
+                    "Probe has stored null command.");
+            }
 
             // Print command (simulate send)
             c.printCommand(this.name);
@@ -167,12 +265,17 @@ public class Probe implements DiagnosticObserver
         }
     }
 
+    /**
+     * Observer method, writes current status of Probe
+     *
+     * @param w DiagnosticWriter object, handles writing of String
+     */
     @Override
     public void write(DiagnosticWriter w)
     {
         // Append status
         String s = String.format("    %s at %.6f %.6f, %s\n", this.name
-            .toUpperCase(), this.currentLat, this.currentLong, this.getState());
+            .toUpperCase(), this.lat, this.longi, this.getState());
         w.append(s);
     }
 }
